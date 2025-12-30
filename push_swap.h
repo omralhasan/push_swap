4a73a7026b02aa11927fc9895810bfcd3433d2b2
Omar Alhasan <oalhasan@c2r1s7.42amman.com>
2025-12-30 18:37:12 +0300

    2

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: oalhasan <oalhasan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/19 18:46:34 by oalhasan          #+#    #+#             */
-/*   Updated: 2025/12/21 18:53:10 by oalhasan         ###   ########.fr       */
+/*   Updated: 2025/12/30 16:51:58 by oalhasan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,5 +41,25 @@ void    rra(sorted_list **a);
 void    rrb(sorted_list **b);
 void    rrr(sorted_list **a, sorted_list **b);
 char	**ft_split(char const *s, char c);
+void	error_exit();
+void	error_free(char **args);
+int	is_valid_number(char *str);
+void	validate_args(char **args);
+void    push(t_stack *stack, t_stack *to);
+int     pop(t_stack *stack);
+int	    is_sorted(t_stack *stack);
+void    free_stack(t_stack *stack);
+void    sort_small_stack(t_stack *stack);
+sorted_list	*new_node(int value);
+void	push_top(t_stack *stack, sorted_list *node);
+void	push_bottom(t_stack *stack, sorted_list *node);
+void	swap(t_stack *stack);
+void	rotate(t_stack *stack);
+void	reverse_rotate(t_stack *stack);
+void	assign_indexes(t_stack *stack);
+void	print_stack(t_stack *stack);
+void sort_two(t_stack *stack);
+void sort_three(t_stack *stack);
+void sort_small(t_stack *stack);
 
 #endif
---
+++ small_sort.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   small_sort.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oalhasan <oalhasan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/27 15:58:10 by oalhasan          #+#    #+#             */
+/*   Updated: 2025/12/30 16:39:15 by oalhasan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+void sort_two(t_stack *stack)
+{       
+    if (stack->top->value > stack->top->next->value)
+        sa(stack);
+}
+void sort_three(t_stack *stack)
+{
+    int first = stack->top->value;
+    int second = stack->top->next->value;
+    int third = stack->top->next->next->value;
+
+    if (first > second && second < third && first < third)
+        sa(stack);
+    else if (first > second && second > third)
+    {
+        sa(stack);
+        rra(stack);
+    }
+    else if (first > second && second < third && first > third)
+        ra(stack);
+    else if (first < second && second > third && first < third)
+    {
+        sa(stack);
+        ra(stack);
+    }
+    else if (first < second && second > third && first > third)
+        rra(stack);
+}
+void sort_small(t_stack *stack)
+{
+    if (stack->size == 2)
+        sort_two(stack);
+    else if (stack->size == 3)
+        sort_three(stack);
+}
+
+void sort_small_stack(t_stack *stack)
+{
+    if (stack->size <= 3)
+        sort_small(stack); 
+}
